dce9da70cdee4369f453a43fceed6fd83eab32bf
henry-jacq <henryeditz267@gmail.com>
2024-03-27 17:24:22 +0530

    change: admin layout style updated

--- views/templates/admin/home.php
+++ views/templates/admin/home.php
@@ -1,54 +1,58 @@
 <style>
-    /* Custom CSS for fixed sidebar and scrollable content */
+    body {
+        overflow: hidden;
+    }
+
     .sidebar {
         position: fixed;
-        top: 0;
+        top: 12px;
         left: -280px;
-        /* Initially hidden off-screen */
         bottom: 0;
         z-index: 100;
-        margin-top: 59px;
         overflow-y: auto;
         width: 280px;
         transition: left 0.5s ease;
-        /* Smooth transition for sliding effect */
     }
 
     .content {
         padding: 20px;
-        padding-top: 70px;
+        overflow-y: auto;
+        max-height: calc(100vh - 59px);
+    }
+
+    .content-container {
+        flex-grow: 1;
+        overflow-y: auto;
+        margin-left: 0;
         transition: margin-left 0.5s ease;
-        /* Smooth transition for content */
     }
 
     @media (min-width: 768px) {
         .sidebar {
             left: 0;
-            /* Always visible on medium and larger screens */
         }
 
-        .content {
+        .content-container {
             margin-left: 280px;
-            /* Adjust content margin for sidebar */
             transition: none;
-            /* No transition for content on larger screens */
         }
     }
 </style>
+
+<nav class="navbar sticky-top border-bottom shadow shadow-bottom navbar-expand-md bg-body-tertiary px-2" aria-label="Fourth navbar example">
+    <div class="container-fluid">
+        <a class="navbar-brand fs-4" href="#">Passito</a>
+        <button class="btn sidebar-toggler me-auto ms-md-5">
+            <i class="bi bi-list-nested"></i>
+        </button>
+        <a role="button" class="text-body-emphasis" href="#">
+            <i class="bi bi-box-arrow-left me-md-1"></i>
+            <span class="d-none d-md-inline-block">Logout</span>
+        </a>
+    </div>
+</nav>
 <div class="d-flex">
-    <nav class="navbar fixed-top border-bottom navbar-expand-md bg-body-tertiary px-2" aria-label="Fourth navbar example">
-        <div class="container-fluid">
-            <a class="navbar-brand fs-4" href="#">Passito</a>
-            <button class="btn sidebar-toggler me-auto ms-md-5">
-                <i class="bi bi-list-nested"></i>
-            </button>
-            <a role="button" class="text-body-emphasis" href="#">
-                <i class="bi bi-box-arrow-left me-md-1"></i>
-                <span class="d-none d-md-inline-block">Logout</span>
-            </a>
-        </div>
-    </nav>
-    <div class="sidebar p-3 border-end bg-body-tertiary text-white">
+    <div class="sidebar mt-5 p-3 bg-body border-end">
         <p class="text-uppercase fw-semibold text-secondary small mb-2 pt-2">Admin Panel</p>
         <ul class="nav nav-pills flex-column">
             <li class="nav-item">
@@ -83,15 +87,21 @@
             </li>
         </ul>
     </div>
-    <div class="content">
-        <h3>Dashboard</h3>
-        <div class="p-5 bg-body-tertiary shadow rounded-3">
-            <h3 class="text-body-emphasis mb-3">Hello Admin! 👋</h3>
-            <p class="">Welcome to Passito! <br> Begin gatepass request approvals now!</p>
+    <div class="content-container">
+        <div class="content">
+            <h3 class="lead fs-3">Dashboard</h3>
+            <hr>
+            <div class="p-5 bg-body-tertiary shadow rounded-3">
+                <h3 class="text-body-emphasis mb-3">Hello Admin! 👋</h3>
+                <p>Welcome to Passito! Begin gatepass request approvals now!</p>
+            </div>
+            <div class="p-5 mt-3 bg-body-tertiary shadow rounded-3">
+                <h3 class="text-body-emphasis mb-3">Description</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, exercitationem tempora quis harum eaque ea dolor dolore odit aut architecto error aspernatur fuga veniam facere at consectetur explicabo porro fugiat. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat cupiditate nisi totam numquam odio. Perspiciatis ea, eius ducimus, id eaque nostrum dolorem non expedita sapiente, necessitatibus delectus quibusdam aperiam nesciunt. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis in eveniet nulla delectus? Facere culpa perferendis magni numquam, dicta voluptatum blanditiis quas tempora quibusdam sequi, ratione veniam ea quisquam dolores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere obcaecati temporibus cupiditate harum dignissimos eligendi cum vero maxime recusandae. Minima vitae dolore quibusdam ipsa aliquid earum nostrum nemo veritatis quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odio obcaecati tempore laudantium, similique optio in ut! Recusandae nam architecto sed officia ullam repellendus eveniet sit hic libero? Doloremque, totam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Id blanditiis ipsam praesentium unde, voluptatum eum laudantium corporis! Nobis, quam nam nulla, dolorum molestias eaque architecto dolores saepe, dignissimos pariatur amet. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti quis iusto ullam consequuntur magnam, excepturi ducimus tempore aliquam beatae recusandae perferendis debitis obcaecati! Ducimus laboriosam distinctio modi alias ipsum quasi!</p>
+            </div>
         </div>
     </div>
 </div>
-
 <script>
     document.addEventListener("DOMContentLoaded", function() {
         var toggler = document.querySelector(".sidebar-toggler");

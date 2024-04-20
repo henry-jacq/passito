2e5d1c65c307fe5f70e68b4e60951ed5d86723e2
henry-jacq <henryeditz267@gmail.com>
2024-04-20 20:51:16 +0530

    new: WYSIWYG design added

--- views/templates/admin/announcements.php
+++ views/templates/admin/announcements.php
@@ -8,7 +8,16 @@
         </div>
         <div class="form-group mb-3">
             <label for="announcementContent" class="form-label">Content</label>
-            <textarea class="form-control" id="announcementContent" rows="5" placeholder="Enter announcement content" required></textarea>
+            <div class="card">
+                <div class="card-header p-2">
+                    <button type="button" class="btn btn-sm border focus-ring focus-ring-prime"><i class="bi bi-type-bold fs-5"></i></button>
+                    <button type="button" class="btn btn-sm border focus-ring focus-ring-prime"><i class="bi bi-type-italic fs-5"></i></button>
+                    <button type="button" class="btn btn-sm border focus-ring focus-ring-prime"><i class="bi bi-type-underline fs-5"></i></button>
+                </div>
+                <div class="card-body p-1">
+                    <textarea class="form-control rounded-0 border-0 px-2 shadow-none" id="announcementContent" rows="6" placeholder="Enter announcement content" required></textarea>
+                </div>
+            </div>
         </div>
         <button type="submit" class="btn btn-prime btn-block">Create</button>
     </form>

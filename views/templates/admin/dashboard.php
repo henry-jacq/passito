bf8e330e313796e8fbe14361d0653831044d7dc3
henry-jacq <henryeditz267@gmail.com>
2024-04-20 23:46:46 +0530

    new: admin dashboard updated

--- views/templates/admin/dashboard.php
+++ views/templates/admin/dashboard.php
@@ -1,6 +1,66 @@
 <h3 class="lead fs-4"><?= $title ?></h3>
 <hr>
-<div class="p-5 bg-body-tertiary shadow rounded-3">
-    <h3 class="text-body-emphasis mb-3">Hello Admin! 👋</h3>
-    <p>Welcome to Passito! Begin gatepass request approvals now!</p>
+<div class="row g-3">
+    <!-- Recent Activities Widget -->
+    <div class="col-md-4 d-flex align-items-stretch">
+        <div class="p-3 border rounded bg-body-emphasis w-100">
+            <h5 class="lead fs-5 mb-3 text-wrap">Recent Activities</h5>
+            <div class="list-group list-group-flush rounded">
+                <a href="#" class="list-group-item bg-transparent">
+                    <strong>08/04/2024 - 12:30 PM</strong>
+                    <br>Request Approved for <i class="link-body-emphasis">2212023</i>
+                </a>
+                <a href="#" class="list-group-item bg-transparent">
+                    <strong>08/04/2024 - 11:45 AM</strong>
+                    <br>Request Denied for <i class="link-body-emphasis">2210057</i>
+                </a>
+                <a href="#" class="list-group-item bg-transparent">
+                    <strong>08/04/2024 - 11:00 AM</strong>
+                    <br>Request Approved for <i class="link-body-emphasis">2210035</i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Upcoming Announcements Widget -->
+    <div class="col-md-4 d-flex align-items-stretch">
+        <div class="p-3 border rounded bg-body-emphasis w-100">
+            <h5 class="lead fs-5 mb-3 text-wrap">Announcements</h5>
+            <div class="list-group list-group-flush rounded">
+                <a href="#" class="list-group-item bg-transparent">
+                    <b>08/04/2024</b><br> Special bus for hostel students
+                </a>
+                <a href="#" class="list-group-item bg-transparent">
+                    <b>08/04/2024</b><br> Mess food chart updated
+                </a>
+                <a href="#" class="list-group-item bg-transparent">
+                    <b>08/04/2024</b><br> Special bus for hostel students
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- System Performance Widget -->
+    <div class="col-md-4 d-flex align-items-stretch">
+        <div class="p-3 border rounded bg-body-emphasis w-100">
+            <h5 class="lead fs-5 mb-3 text-wrap">System Performance</h5>
+            <div class="list-group list-group-flush rounded">
+                <a href="#" class="list-group-item bg-transparent">
+                    <strong>Active Users:</strong> 150
+                </a>
+                <a href="#" class="list-group-item bg-transparent">
+                    <strong>CPU Usage:</strong> 35%
+                </a>
+                <a href="#" class="list-group-item bg-transparent">
+                    <strong>Memory Usage:</strong> 45%
+                </a>
+                <a href="#" class="list-group-item bg-transparent">
+                    <strong>Load Time:</strong> 1.2 sec
+                </a>
+                <a href="#" class="list-group-item bg-transparent">
+                    <strong>System Uptime:</strong> 99.9%
+                </a>
+            </div>
+        </div>
+    </div>
+
 </div>

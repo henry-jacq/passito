f6ca589cd48008e21b504aeb0be7e4075ace8a74
henry-jacq <henryeditz267@gmail.com>
2024-05-12 11:39:23 +0530

    fix: not loading admin layout bug fixed

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -25,12 +25,16 @@ class Controller
     
     public function render(Request $request, Response $response, string $viewPath, array $args, $header = true, $footer = false)
     {
-        $role = $request->getAttribute('role');
-        
-        if ($role != "admin") {
-            $args['header'] = $header;
+        // $role = $request->getAttribute('role');
+        $role = "admin";
+
+        if ($role == "admin") {
+            $args['role'] = $role;
+        } else {            $args['header'] = $header;
             $args['footer'] = $footer;
         }
+
+        // dd($args);
         
         $response->getBody()->write(
             (string) $this->view

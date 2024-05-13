2ec4649c4224c6eacc2ed6229a99a03622da1537
henry-jacq <henryeditz267@gmail.com>
2024-05-13 19:38:32 +0530

    change: new fields added to compose mail section

--- views/templates/admin/compose.php
+++ views/templates/admin/compose.php
@@ -10,6 +10,39 @@
         <textarea class="form-control" id="body" rows="6" name="body" placeholder="Enter email body" required></textarea>
     </div>
     <div class="mb-3">
+        <label class="form-label">Select Institution(s)</label>
+        <br>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="institution1" value="institution1">
+            <label class="form-check-label" for="institution1">SSN</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="institution2" value="institution2">
+            <label class="form-check-label" for="institution2">SNU</label>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="recipients" class="form-label">Recipients</label>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="year1" value="year1">
+            <label class="form-check-label" for="year1">1st Year Students</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="year2" value="year2">
+            <label class="form-check-label" for="year2">2nd Year Students</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="year3" value="year3">
+            <label class="form-check-label" for="year3">3rd Year Students</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="year4" value="year4">
+            <label class="form-check-label" for="year4">4th Year Students</label>
+        </div>
+    </div>
+
+
+    <div class="mb-3">
         <label for="recipients" class="form-label">Recipient</label>
         <input type="text" class="form-control" id="recipients" name="recipient" placeholder="Enter recipient email" required>
         <div id="recipientsHelp" class="form-text">Enter a single email address.</div>

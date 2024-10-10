1c873c20727441a25130543769993738c3b3a00c
henry-jacq <henryeditz267@gmail.com>
2024-10-10 07:57:52 +0000

    refactor: Add AppEnvironment enum for better app environment handling

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enum\AppEnvironment;
+
 $boolean = function (mixed $value) {
     if (in_array($value, ['true', 1, '1', true, 'yes'], true)) {
         return true;
@@ -10,23 +12,30 @@ $boolean = function (mixed $value) {
 
 $appSnakeName = strtolower(str_replace(' ', '_', $_ENV['APP_NAME']));
 
+$appEnv = $_ENV['APP_ENV'] ?? AppEnvironment::Production->value;
+
 return [
     'app' => [
-        'name' => $_ENV['APP_NAME'],
-        'desc' => $_ENV['APP_DESC'],
-        'host' => $_ENV['APP_URL'] ?? 'http://localhost',
+        'name' => $_ENV['APP_NAME'] ?? 'Passito',
+        'desc' => $_ENV['APP_DESC'] ?? '',
+        'host' => $_ENV['APP_URL'] ?? 'http://passito.local',
         'version' => $_ENV['APP_VERSION'] ?? '1.0',
         'display_error_details' => $boolean($_ENV['APP_DEBUG'] ?? 0),
         'log_errors' => true,
         'log_error_details' => true,
     ],
-    'db' => [
-        'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
-        'host' => $_ENV['DB_HOST'],
-        'port' => $_ENV['DB_PORT'] ?? 3306,
-        'dbname' => $_ENV['DB_NAME'],
-        'user' => $_ENV['DB_USER'],
-        'pass' => $_ENV['DB_PASS'],
+    'doctrine' => [
+        'dev_mode' => AppEnvironment::isDevelopment($appEnv) ?? 'development',
+        'cache_dir' => STORAGE_PATH . '/cache/doctrine',
+        'entity_dir' => [APP_PATH . '/Entity'],
+        'connection' => [
+            'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
+            'host' => $_ENV['DB_HOST'],
+            'port' => $_ENV['DB_PORT'] ?? 3306,
+            'dbname' => $_ENV['DB_NAME'],
+            'user' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASS'],
+        ]
     ],
     'mail' => [
         'host' => $_ENV['SMTP_HOST'],

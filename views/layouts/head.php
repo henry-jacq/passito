72463fdbfcd5c6f908e9ba9e00a8d1bdf0f5fe91
henry-jacq <henryeditz267@gmail.com>
2024-10-11 05:44:37 +0000

    Remove unused CSS and JavaScript files

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -3,7 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?? $this->title ?> · <?= $this->appName ?></title>
-    <link rel="icon" type="image/x-icon" href="/assets/brand/photogram-icon.png">
+    <!-- <link rel="icon" type="image/x-icon" href="/assets/brand/photogram-icon.png"> -->
     <meta name="author" content="Henry">
     <meta property="og:image" content="/assets/brand/photogram-icon.png">
     <meta property="site_name" content="<?= $this->appName ?>">
@@ -16,10 +16,7 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="<?= $this->appName ?>">
     <meta name="twitter:description" content="<?= $this->appName ?> is an easy-to-use web app for applying gatepass instantly.">
-    <meta name="twitter:image" content="/assets/screenshot-2.png">
+    <!-- <meta name="twitter:image" content="/assets/screenshot-2.png"> -->
     <link rel="shortcut icon" href="/assets/brand/favicon.ico">
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link href="/css/app.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/dist/assets/main-BfibREyH.css">
 </head>

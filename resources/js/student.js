2e85e53f404e2db866b20aff34f0276ebfa7aefa
henry-jacq <henryeditz267@gmail.com>
2025-01-29 13:01:23 +0000

    refactor: toggle purpose field based on outpass type

--- resources/js/student.js
+++ resources/js/student.js
@@ -1,4 +1,5 @@
 import Ajax from './libs/ajax.js';
+import Toast from './libs/toast.js';
 
 // Mobile Menu Toggle Logic
 const mobileMenuToggle = document.getElementById('mobileMenuToggle');
@@ -25,6 +26,13 @@ outpassForm.addEventListener('submit', async (event) => {
 	outpassSubmitButton.innerHTML = 'Submitting...'
     const formData = new FormData(outpassForm);
 
+	formData.forEach((value, key) => {
+		// If the value is empty (null, undefined, or an empty string)
+		if (!value) {
+			formData.set(key, null); // Set it as null
+		}
+	});
+
     try {
 		const response = await Ajax.post('/api/web/student/outpass', formData);
 		
@@ -36,10 +44,30 @@ outpassForm.addEventListener('submit', async (event) => {
 		}
 		
 		outpassSubmitButton.disabled = false
-		outpassSubmitButton.innerHTML = 'Submit'
+		outpassSubmitButton.innerHTML = 'Submit Request'
 
     } catch (error) {
         console.error('Error during submission:', error);
         alert('Failed to Submit Outpass Request. Please try again.');
     }
+});
+
+
+// Toggle Purpose Field
+const outpassType = document.getElementById('outpass_type');
+
+outpassType.addEventListener('change', () => {
+	const selectedValue = outpassType.value;
+	const purposeInput = document.getElementById('purpose');
+	const purposeField = document.getElementById('purposeField');
+	
+	if (selectedValue == 'home') {
+		purposeField.classList.add("hidden");
+		purposeField.classList.remove("block");
+		purposeInput.removeAttribute("required");
+	} else {
+		purposeField.classList.add("block");
+		purposeField.classList.remove("hidden");
+		purposeInput.setAttribute("required", "required");
+	}
 });
--- views/templates/user/request_outpass.php
+++ views/templates/user/request_outpass.php
@@ -37,6 +37,12 @@
                     </div>
                 </div>
 
+                <!-- Destination -->
+                <div>
+                    <label for="destination" class="block text-gray-700 font-medium">Destination</label>
+                    <input type="text" name="destination" id="destination" required class="mt-1 w-full rounded-md border-gray-300 focus:border-purple-500 focus:ring focus:ring-purple-200 transition" placeholder="Enter Destination">
+                </div>
+
                 <!-- Outpass Type -->
                 <div>
                     <label for="outpass_type" class="block text-gray-700 font-medium">Outpass Type</label>
@@ -49,20 +55,14 @@
                 </div>
 
                 <!-- Purpose -->
-                <div>
+                <div id="purposeField" class="hidden">
                     <label for="purpose" class="block text-gray-700 font-medium">Purpose</label>
                     <textarea name="purpose" id="purpose" rows="3" required class="mt-1 w-full rounded-md border-gray-300 focus:border-purple-500 focus:ring focus:ring-purple-200 transition" placeholder="Describe the purpose of your outpass"></textarea>
                 </div>
 
-                <!-- Location -->
-                <div>
-                    <label for="destination" class="block text-gray-700 font-medium">Destination</label>
-                    <input type="text" name="destination" id="destination" required class="mt-1 w-full rounded-md border-gray-300 focus:border-purple-500 focus:ring focus:ring-purple-200 transition" placeholder="Enter destination">
-                </div>
-
                 <!-- Submit Button -->
                 <div class="flex justify-end">
-                    <button id="outpassSubmitButton" type="submit" class="px-4 py-2 bg-purple-600 text-lg text-white rounded-lg shadow hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500">
+                    <button id="outpassSubmitButton" type="submit" class="px-4 py-2 bg-purple-600 text-md text-white rounded-lg shadow hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500">
                         Submit Request
                     </button>
                 </div>

9c9c5bc482a4878f369ed357cf52b0ccc6f80b96
henry-jacq <henryeditz267@gmail.com>
2025-01-30 13:16:59 +0000

    refactor: applied tailwind styles to  error page

--- views/templates/error.php
+++ views/templates/error.php
@@ -1,11 +1,13 @@
-<div class="d-flex align-items-center justify-content-center min-vh-100">
-    <div class="p-md-5 py-4">
-        <div class="row mx-auto text-center py-5 bg-body-tertiary rounded">
-            <span class="display-2 d-block"><?= $code ?></span>
-            <span class="display-4 d-block mb-3">Error</span>
-            <div class="mb-4 lead px-sm-5">The page you are looking for was not found or other error occured.</div>
-            <div class="d-grid col-sm-8 col-md-5 col-lg-3 mx-auto">
-                <a href="/" class="btn text-body border-0 text-decoration-none hvr-icon-back"><i class="fa fa-arrow-left me-2 hvr-icon"></i>Go Back</a>
+<div class="flex items-center justify-center min-h-screen">
+    <div class="p-6 md:p-10">
+        <div class="flex flex-col items-center justify-center py-12 bg-gray-100 rounded-lg text-center">
+            <span class="text-6xl block"><?= $code ?></span>
+            <span class="text-4xl block mb-3">Error</span>
+            <div class="mb-4 text-lg px-5 sm:px-10">The page you are looking for was not found or other error occurred.</div>
+            <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 mx-auto mt-4">
+                <a href="<?= $this->urlFor('auth.login') ?>" class="btn text-body border-0 text-decoration-none hover:bg-gray-200 px-6 py-2 rounded-md transition duration-300">
+                    <i class="fa fa-arrow-left mr-2"></i>Go Back
+                </a>
             </div>
         </div>
     </div>
